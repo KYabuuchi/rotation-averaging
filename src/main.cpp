25a5f3b6b6f01339db3bca99bd830280cc7f4d3c
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2020-07-16 13:52:13 +0900

    draw transparent cone

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,14 @@ void publishCone(ros::Publisher& publisher)
 {
   Eigen::Matrix4d T;
   T.Identity();
-  double angle = 3.141592 / 4;
-  double length = 1;
 
   visualization_msgs::Marker marker;
   marker.header.frame_id = "world";
   marker.header.stamp = ros::Time();
-  marker.ns = "hoge";
+  marker.ns = "cone";
   marker.id = 0;
   marker.type = visualization_msgs::Marker::TRIANGLE_LIST;
   marker.action = visualization_msgs::Marker::ADD;
-  marker.pose.position.x = 0.0;
-  marker.pose.position.y = 0.0;
-  marker.pose.position.z = 0.0;
   marker.pose.orientation.x = 0.0;
   marker.pose.orientation.y = 0.0;
   marker.pose.orientation.z = 0.0;
@@ -41,47 +36,55 @@ void publishCone(ros::Publisher& publisher)
   marker.color.b = 1.0;
   marker.color.a = 1.0;
 
-  {
+  const int N = 8;
+  std_msgs::ColorRGBA color[3];
+  color[0].r = 1.0;
+  color[0].g = 0.2;
+  color[0].b = 0.2;
+  color[0].a = 0.2;
+  color[1].r = 0.2;
+  color[1].g = 1.0;
+  color[1].b = 0.2;
+  color[1].a = 0.2;
+  color[2].r = 0.2;
+  color[2].g = 0.2;
+  color[2].b = 1.0;
+  color[2].a = 0.2;
+
+
+  auto convert = [](const Eigen::Vector3d& v) -> geometry_msgs::Point {
     geometry_msgs::Point p;
-    p.x = 0;
-    p.y = 0;
-    p.z = 0;
-    marker.points.push_back(p);
-    p.y = 0.2;
-    marker.points.push_back(p);
-    p.z = 0.2;
-    marker.points.push_back(p);
+    p.x = v.x();
+    p.y = v.y();
+    p.z = v.z();
+    return p;
+  };
 
-    std_msgs::ColorRGBA c;
-    c.r = 0.1;
-    c.g = 1.0;
-    c.b = 0.1;
-    c.a = 0.9;
-    marker.colors.push_back(c);
-    marker.colors.push_back(c);
-    marker.colors.push_back(c);
+  const double L = 0.3;  // length
+  const double R = 0.1;  // radius
+
+  for (int axis = 0; axis < 3; axis++) {
+
+    Eigen::Vector3d s(0, 0, 0);
+    Eigen::Vector3d t(0, 0, 0);
+    t(axis) = L;
+    Eigen::Vector3d n(0, 0, 0);
+    n((axis + 1) % 3) = R;
+    Eigen::Vector3d m(0, 0, 0);
+    m((axis + 2) % 3) = R;
+
+    Eigen::Vector3d nm = std::cos(2 * (N - 1) * M_PI / N) * n + std::sin(2 * (N - 1) * M_PI / N) * m;
+    for (int i = 0; i < N; i++) {
+      marker.points.push_back(convert(s));
+      marker.points.push_back(convert(t + nm));
+      nm = std::cos(2 * i * M_PI / N) * n + std::sin(2 * i * M_PI / N) * m;
+      marker.points.push_back(convert(t + nm));
+
+      marker.colors.push_back(color[axis]);
+      marker.colors.push_back(color[axis]);
+      marker.colors.push_back(color[axis]);
+    }
   }
-  {
-    geometry_msgs::Point p;
-    p.x = 0.5;
-    p.y = 0;
-    p.z = 0;
-    marker.points.push_back(p);
-    p.y = 0.2;
-    marker.points.push_back(p);
-    p.z = 0.2;
-    marker.points.push_back(p);
-
-    std_msgs::ColorRGBA c;
-    c.r = 1.0;
-    c.g = 0.1;
-    c.b = 0.1;
-    c.a = 0.9;
-    marker.colors.push_back(c);
-    marker.colors.push_back(c);
-    marker.colors.push_back(c);
-  }
-
   publisher.publish(marker);
 }
 
