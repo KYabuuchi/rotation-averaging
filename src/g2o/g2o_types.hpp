d50182c2aa93e8b04b536177947c5cf6f7446e4a
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2020-07-19 16:24:36 +0900

    analytical jacobian

--- src/g2o/g2o_types.hpp
+++ src/g2o/g2o_types.hpp
@@ -37,8 +37,23 @@ public:
     _error(0) = (so3::exp(vp0->estimate()) * measurement() - so3::exp(vp1->estimate())).norm();
   }
 
-  // Analytical Jacobian
-  // virtual void linearizeOplus()
-  // {
-  // }
+  virtual void linearizeOplus()
+  {
+    VertexRotation* vp0 = static_cast<VertexRotation*>(vertex(0));
+    VertexRotation* vp1 = static_cast<VertexRotation*>(vertex(1));
+    Eigen::Matrix3d R0 = so3::exp(vp0->estimate());
+    Eigen::Matrix3d R1 = so3::exp(vp1->estimate());
+    Eigen::Matrix3d R01 = measurement();
+
+    Eigen::Matrix3d Ri = -R1 * R01.transpose() * R0.transpose();
+    Eigen::Matrix3d Rj = -R0 * R01 * R1.transpose();
+
+    _jacobianOplusXi(0, 0) = -Ri(1, 2) + Ri(2, 1);
+    _jacobianOplusXi(0, 1) = Ri(0, 2) - Ri(2, 0);
+    _jacobianOplusXi(0, 2) = -Ri(0, 1) + Ri(1, 0);
+
+    _jacobianOplusXj(0, 0) = -Rj(1, 2) + Rj(2, 1);
+    _jacobianOplusXj(0, 1) = Rj(0, 2) - Rj(2, 0);
+    _jacobianOplusXj(0, 2) = -Rj(0, 1) + Rj(1, 0);
+  }
 };
